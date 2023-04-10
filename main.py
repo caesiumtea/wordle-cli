9d2ad4a3cc1b581b02a2aed9a5a7b0d3bf84a45c
vance <caesiumtea@gmail.com>
2023-04-09 23:17:51 -0400

    Count tries

--- main.py
+++ main.py
@@ -1,36 +1,49 @@
 from random import choice
 import requests
 
+commonWords = ["wordy"]
+allWords = ["wordy", "truth", "ghast", "volts", "swing", "swish", "splat", "smash"]
 # commonWords = read small wordlist from github
 # allWords = read large wordlist from local file
 
+maxTries = 6
 wordLength = 5
 # pare down word lists to wordlength words only?
+#TODO: helper function to generate starting board dynamically based on wordLength
 
 # helper function to draw game board?
 
 def play():
-  #TODO: generate starting board dynamically based on wordLength
   board = ["         _  _  _  _  _ "]
   solution = choice(commonWords) #choose a random word from short list
+  tries = 0
 
   # print intro
   # ask if they want instructions
 
-  while True:
+  while tries < maxTries:
     response = ""
-    guess = input("Guess a word: ")
-    if guess == "!help":
+
+    triesLeft = maxTries - tries
+    print(f"You have {triesLeft} more tries to guess the secret word.")
+    guess = input("Guess a word: ").upper()
+
+    if guess == "!HELP":
+      print("help")
       # print instructions 
       # print board
-    elif guess == "!letters":
+    elif guess == "!LETTERS":
+      print("letters")
       # show letters guessed so far
       # print board
+    elif guess == "!QUIT":
+      break
     elif guess == solution:
       print("You win!")
       break
     #TODO: make this a helper function to process valid guesses
-    elif len(guess) == wordLength and guess in allWords:
+    elif len(guess) == wordLength and guess.lower() in allWords:
+      tries += 1
       response += guess + " | "
       for i in range(wordLength):
         if guess[i] == solution[i]: 
@@ -44,6 +57,8 @@ def play():
       board.append(response)
     else:
       print(f"Guess must be a real {wordLength}-letter word.")
+  if tries >= maxTries:
+    print("No guesses left.")
   return
 
 play()
